5cf94c9cd6265af54f63fef5f5a9f9cc444d238d
Katerina Dimitrova <158329994+cathy-09@users.noreply.github.com>
2026-07-22 19:09:35 +0300

    Add word length counting functionality
    Implement function to count even and odd word lengths in a string.

---
+++ Cathy/2020_exam_problems_summer/task_2.cpp
@@ -0,0 +1,64 @@
+#include <iostream>
+using namespace std;
+int* numOfWords(char str[]);
+const int SIZE = 2;
+
+int main()
+{
+	char s1[] = " This is an example string ";
+	int* wordsRes = numOfWords(s1);
+	if (wordsRes)
+	{
+		cout << "Word lengths {even, odd}: {" << wordsRes[0] << ", " << wordsRes[1] << "} (Expected: {4, 1})" << endl;
+		delete[] wordsRes;
+	}
+	cout << endl;
+}
+
+int* numOfWords(char str[])
+{
+	if (str == nullptr)
+	{
+		return nullptr;
+	}
+	int chetnoCount = 0;
+	int nechetnoCount = 0;
+	int currCount = 0;
+	int i = 0;
+	for (size_t i = 0; str[i] != '\0'; i++)
+	{
+		char charche = str[i];
+		if (charche != ' ' && charche != '\t')
+		{
+			currCount++;
+		}
+		else if (currCount > 0)
+		{
+			if (currCount % 2 == 0)
+			{
+				chetnoCount++;
+			}
+			else
+			{
+				nechetnoCount++;
+			}
+			currCount = 0;
+		}
+	}
+	if (currCount > 0)
+	{
+		if (currCount % 2 == 0)
+		{
+			chetnoCount++;
+		}
+		else
+		{
+			nechetnoCount++;
+		}
+	}
+	int* result = new int[SIZE];
+	result[0] = chetnoCount;
+	result[1] = nechetnoCount;
+
+	return result;
+}
